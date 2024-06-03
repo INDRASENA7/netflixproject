bcb557309889d5600fd197195fb5b8fc41902d58
Your Name <you@example.com>
2024-06-03 23:28:52 +0530

    first commit

---
+++ src/Apis/api.jsx
@@ -0,0 +1,16 @@
+const API_KEY='e369df88d802090485867892311042b8'
+
+
+
+const api={
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixoriginals:`/discover/tv?api_key=${API_KEY}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc`,
+    fetchComedymovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchRomanticmovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchActionmovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchHorrormovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTopratedmovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+}
+
+export default api
